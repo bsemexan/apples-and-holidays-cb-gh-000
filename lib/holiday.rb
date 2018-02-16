8410c088a7897e2a2613662eb7b6ab1139c48f20
Berry  Semexan <bsemexan@gmail.com>
2018-02-16 23:36:09 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |key, season|
-    puts "#{key.capitalize}"
+    puts "#{key.capitalize:}"
   end
 
 end
