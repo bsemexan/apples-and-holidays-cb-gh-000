aa99f48a7f2dae51561a2a5247fecd9ece46136e
Berry  Semexan <bsemexan@gmail.com>
2018-02-16 23:24:50 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,6 +62,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  has_bbq = []
   holiday_hash.each do |key, season|
     season.each do |holi_name, supply|
       holi_name if supply.include?("BBQ")
